8daa45dd609c7572ee70455ced6eebd35e413c45
ygeyzel <ygeyzel@gmail.com>
2025-10-09 00:46:59 +0300

    fake midi controller

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from inputs.inputmanager import MidiInputManager
 from scenes.deadmau5_fractal import Deadmau5Fractal
 from scenes.kepler_planet import KeplerPlanet
 from scenes.scene import Scene
+from utils.fakecontroller import FakeController
 
 
 MIDI_INPUT_SUBNAME = "Mixage"
@@ -24,11 +25,10 @@ class Screen(mglw.WindowConfig):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         
-        # Current key being pressed (None when no key is pressed)
-        self.key = None
-
         # Initialize input handling system
-        self.inputmanager = MidiInputManager(MIDI_INPUT_SUBNAME)
+        self.fake_controller = FakeController()
+        self.inputmanager: MidiInputManager
+        self.init_input_manager()
         
         # Initialize the scene (can be easily swapped for different scenes)
         self.scene: Scene = Deadmau5Fractal(self.ctx, self.resource_dir)
@@ -40,12 +40,19 @@ class Screen(mglw.WindowConfig):
         for param in self.scene.params:
             self.inputmanager.bind_param(param, param.button.value)
 
+    def init_input_manager(self):
+        input_subname = self.fake_controller.output_name if self.fake_controller else MIDI_INPUT_SUBNAME
+        self.inputmanager = MidiInputManager(input_subname)
+
     def on_render(self, time: float, frame_time: float):
         """Main render loop - called every frame by moderngl-window"""
 
         # Delegate rendering to the current scene
         resolution = (self.wnd.width, self.wnd.height, 1.0)
         self.scene.render(time, frame_time, resolution)
+
+    def on_key_event(self, key, action, modifiers):
+        pass
 
 
 if __name__ == "__main__":
