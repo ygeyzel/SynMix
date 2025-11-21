49b5a550f74d2f4ab35b8c934ae70453e665abe4
Tsvika Shapira <tsvikas@gmail.com>
2025-11-21 03:02:49 +0200

    format

--- resources/shaders/triskelion.glsl
+++ resources/shaders/triskelion.glsl
@@ -8,40 +8,49 @@ out vec4 fragColor;
 uniform vec3 iResolution;
 uniform float iTime;
 
-
 // Neon circles blooming into a fractal hexagonal grid.
 // Inspiration:
-// - https://www.shadertoy.com/view/mly3Dd - Glowing concentric circles + square grid folding
+// - https://www.shadertoy.com/view/mly3Dd - Glowing concentric circles + square
+// grid folding
 // - https://www.shadertoy.com/view/NtBSRV - Hexagonal cells
 
-//#define TETRASKELION  // if hexagons offend you
+// Biased rainbow color map. Will be squared later.
+vec3 pal(float a) { 
+  return 0.5 + cos(3.0 * a + vec3(2, 1, 0)); 
+}
 
-vec3 pal(float a) { return 0.5 + cos(3.0*a + vec3(2,1,0)); }  // Biased rainbow color map. Will be squared later.
-
-vec2 fold(vec2 p) {  // Shift and fold into a vertex-centered grid.
-#ifdef TETRASKELION
-  return fract(p) - 0.5;
-#else
-  vec4 m = vec4(2,-1, 0,sqrt(3.));
-  p.y += m.w/3.0;      // center at vertex
-  vec2 t = mat2(m)*p;  // triangular coordinates (x →, y ↖, x+y ↗)
-  return p - 0.5*mat2(m.xzyw) * round((floor(t) + ceil(t.x+t.y)) / 3.0);  // fold into hexagonal cells
-#endif
+// Shift and fold into a vertex-centered grid.
+vec2 fold(vec2 p) {
+  vec4 m = vec4(2, -1, 0, sqrt(3.));
+  // center at vertex
+  p.y += m.w / 3.0;
+  // triangular coordinates (x →, y ↖, x+y ↗)
+  vec2 t = mat2(m) * p;
+  // fold into hexagonal cells
+  return p - 0.5 * mat2(m.xzyw) * round((floor(t) + ceil(t.x + t.y)) / 3.0);
 }
 
 void main() {
-  float t = iTime / 4.0, t2 = t * 0.618034, t3 = t * 1.4142135;  // dissonant timers
-  mat2 M = mat2(cos(t),sin(t), -sin(t),cos(t)) * (1.0 - 0.1*cos(t2));  // rotation and scale: 0.9 [smooth] .. 1.1 [fractal]
+  // dissonant timers
+  float t = iTime / 4.0, t2 = t * 0.618034, t3 = t * 1.4142135;
+  // rotation and scale: 0.9 [smooth] .. 1.1 [fractal]
+  mat2 M = mat2(cos(t), sin(t), -sin(t), cos(t)) * (1.0 - 0.1 * cos(t2));
 
-  vec2 p = (2.0*fragCoord - iResolution.xy) / iResolution.y;  // y: -1 .. 1
-  float d = 0.5*length(p);  // animation phase is based on distance to center
+  // y: -1 .. 1
+  vec2 p = (2.0 * fragCoord - iResolution.xy) / iResolution.y;
+  // animation phase is based on distance to center
+  float d = 0.5 * length(p);
 
   vec3 sum = vec3(0);
   for (float i = 0.0; i < 24.0; i++) {
-    p = fold(M * p);                                            // rotate and scale, fold
-    sum += pal(0.01*i - d + t2) / cos(d - t3 + 5.0*length(p));  // interfering concentric circles
-    // Use pal(...)/abs(cos(...)) for additive circles. I like the interference effect without the abs.
+    // rotate and scale, fold
+    p = fold(M * p); 
+    // interfering concentric circles
+    sum += pal(0.01 * i - d + t2) / cos(d - t3 + 5.0 * length(p));
+    // Use pal(...)/abs(cos(...)) for additive circles. I like the interference
+    // effect without the abs.
   }
-  
-  fragColor = vec4(0.0002*sum*sum, 1);  // square the sum for better contrast
+
+  // square the sum for better contrast
+  fragColor = vec4(0.0002 * sum * sum, 1);
 }
