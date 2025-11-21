2b6b390a8f3921ade40b49882ce748239b3fae22
Tsvika Shapira <tsvikas@gmail.com>
2025-11-21 02:00:50 +0200

    add controls

--- resources/shaders/neon_kaleidoscope.glsl
+++ resources/shaders/neon_kaleidoscope.glsl
@@ -5,13 +5,25 @@ out vec4 fragColor;
 uniform vec3 iResolution;
 uniform float iTime;
 
-vec3 palette(float t) {
-        vec3 a = vec3(0.388, 0.508, 0.500);
-        vec3 b = vec3(-0.392, 0.488, 0.428);
-        vec3 c = vec3(1.000, 1.000, 1.000);
-        vec3 d = vec3(1.000, 1.328, 1.268);
+uniform float phase_r;
+uniform float phase_g;
+uniform float phase_b;
 
-        return a + b * cos(6.28318 * (c * t * d));
+uniform float iMax; 
+
+uniform float w_d; 
+uniform float time_dilation;
+uniform float uv_factor;
+uniform float uvi_factor;
+
+
+vec3 palette(float t) {
+    vec3 a = vec3(0.5, 0.5, 0.5);
+    vec3 b = vec3(0.5, 0.5, 0.5);
+    vec3 c = vec3(1.0, 1.0, 1.0);
+    vec3 d = vec3(phase_r,phase_g,phase_b);
+
+    return a + b*cos( 6.28318*(c*t+d) );
 }
 
 void main()
@@ -19,21 +31,18 @@ void main()
         vec2 uv = (fragCoord * 2.0 - iResolution.xy) / iResolution.y;
         vec2 uv0 = uv;
         vec3 finalColor = vec3(0.0);
-
-        for (float i = 0.0; i < 7.0; i += 1.0) {
-                uv = fract(uv * (0.5 + i / 3.0)) - 0.5;
+        for (float i = 0.0; i < iMax; i += 1.0) {
+                float time_tag = iTime*(1 + time_dilation * i / 15.0);
+                uv = fract(uv * (uv_factor + uvi_factor*i/iMax)) - 0.5;
 
                 float d = length(uv) * exp(-length(uv0));
-
-                vec3 col = palette(length(uv0) + (iTime / 15.0 * i));
-
-                d = sin(d * 8. + sin(iTime / (1.0 + i)) + iTime) / 8.;
+                d = sin(d * w_d + time_tag) / 8.;
                 d = abs(d);
-                d = pow(0.01 / d, 1.5);
+                d = pow(0.01 / d, 1.5)/iMax*4.;
 
-                col *= d;
+                vec3 col = palette(length(uv0) + time_tag);
 
-                finalColor += col;
+                finalColor += col * d;
         }
 
         fragColor = vec4(finalColor, 1.0);
