a097da5d5bbf2f2ed7f85f4d4f50aba5886859c8
Tsvika Shapira <tsvikas@gmail.com>
2025-11-23 19:45:57 +0200

    newscene - julia sets

---
+++ resources/shaders/julia.glsl
@@ -0,0 +1,97 @@
+#version 330
+// based on https://www.shadertoy.com/view/MtjSRw
+
+in vec2 fragCoord;
+out vec4 fragColor;
+uniform vec3 iResolution;
+
+// params
+uniform float x;
+uniform float y;
+uniform float n;
+
+const int MAX_ITERATIONS = 128;
+
+struct complex {
+  float real;
+  float imaginary;
+};
+
+float fractal(complex c, complex z, float n) {
+  for (int iteration = 0; iteration < MAX_ITERATIONS; iteration++) {
+    // z <- z^n + c
+    float s = pow(z.real * z.real + z.imaginary * z.imaginary, n / 2.);
+    float a = float(n) * atan(z.imaginary, z.real);
+
+    z.real = s * cos(a) + c.real;
+    z.imaginary = s * sin(a) + c.imaginary;
+
+    float abs_z_2 = z.real * z.real + z.imaginary * z.imaginary;
+    if (abs_z_2 > 4.0) {
+      float c = float(iteration + 1) - log(log(abs_z_2)) / log(n);
+      return c / float(MAX_ITERATIONS);
+    }
+  }
+
+  return 0.0;
+}
+
+float mandelbrot(vec2 coordinate, float n) {
+  complex c = complex(coordinate.x, coordinate.y);
+  complex z = complex(0.0, 0.0);
+
+  return fractal(c, z, n);
+}
+
+float julia(vec2 coordinate, vec2 offset, float n) {
+  complex c = complex(offset.x, offset.y);
+  complex z = complex(coordinate.x, coordinate.y);
+
+  return fractal(c, z, n);
+}
+
+vec2 fragCoordToXY(vec2 fragCoord) {
+  vec2 relativePosition = fragCoord.xy / iResolution.xy;
+  float aspectRatio = iResolution.x / iResolution.y;
+
+  vec2 cartesianPosition = (relativePosition - 0.5) * 2.0 * 2.0;
+  cartesianPosition.x *= aspectRatio;
+
+  return cartesianPosition;
+}
+
+vec3 juliaColor(float v) {
+  float v0 = pow(v, 0.75);
+  return vec3(v0, 0.0, 0.0);
+}
+
+vec3 mandelbrotColor(float v) {
+  float v0 = pow(v, 0.75);
+  return vec3(0.0, v0, 0.0);
+}
+
+vec3 clickPointColor(float v) {
+  return vec3(0.0, 0.0, 1.0*v);
+}
+
+void mainImage(out vec4 fragColor, in vec2 fragCoord) {
+  vec2 coordinate = fragCoordToXY(fragCoord);
+  vec2 coordinate2 = fragCoordToXY(fragCoord);
+  vec2 clickPosition = vec2(x, y);
+
+  float juliaValue = julia(coordinate2, clickPosition, n);
+  float mandelbrotValue = mandelbrot(coordinate, n);
+
+  float clickPoint =
+      smoothstep(0.02 * 1.1, 0.02, length(clickPosition - coordinate));
+
+  fragColor = vec4(
+    mandelbrotColor(mandelbrotValue)
+    + juliaColor(juliaValue)
+    + clickPointColor(clickPoint),
+    1.0);
+}
+
+void main() {
+  mainImage(fragColor, fragCoord);
+}
